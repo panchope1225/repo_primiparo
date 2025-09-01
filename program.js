3f054c2a9e9416b8d3b25b473add9b0b87b9a94e
joan David Torres <jdtsarmiento9546@gmail.com>
2025-09-01 15:49:41 -0600

    v 0.2

--- program.js
+++ program.js
@@ -1,6 +1,5 @@
 var map = L.map('map').setView([4.633726760990255, -74.14290923566787], 13);
 
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
