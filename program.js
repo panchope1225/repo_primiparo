c2cae49ae40524136c2f921ccaa93f5547737915
Joan David Torres <jdtsarmiento9546@gmail.com>
2025-09-10 15:46:28 -0600

    v 0.3 nose aaa

--- program.js
+++ program.js
@@ -5,5 +5,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([4.633726760990255, -74.14290923566787]).addTo(map);
-var marker = L.marker([4.636180535043864, -74.13749822334897]).addTo(map);
+var marker = L.marker([4.633255, -74.1429787]).addTo(map);
+var marker = L.marker([4.6361803864, -74.13546385465]).addTo(map);
